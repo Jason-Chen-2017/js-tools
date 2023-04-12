a07cee412d4ad98b6700c6cf9684f5d0372063e2
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-04-13 01:56:20 +0800

    20230413

--- temp.java
+++ temp.java
@@ -1,45 +1,24 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Collections;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String str_0 = scan.nextLine().trim();
-        int n = Integer.parseInt(str_0);
-        ArrayList<ArrayList<Integer>> vector = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String str_1 = scan.nextLine();
-            String[] line_list_1 = str_1.trim().split(" ");
-            ArrayList<Integer> temp_1 = new ArrayList<>();
-            for (int j = 0; j < line_list_1.length; j++) {
-                temp_1.add(Integer.parseInt(line_list_1[j]));
-            }
-            vector.add(temp_1);
-        }
-        scan.close();
-        int result = solution(n, vector);
-        out.println(result);
-    }
 
-    public static int solution(int n, ArrayList<ArrayList<Integer>> vector) {
-        int result = 0;
-        int[] row = new int[1005];
-        int[] col = new int[1005];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int g = vector.get(i).get(j);
-                row[i] += g;
-                col[j] += g;
-            }
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int d = Math.abs(a * a - b * b);
+        int f = Math.abs(a * a + b * b);
+
+        int r = (int) Math.sqrt(d);
+        int s = (int) Math.sqrt(f);
+
+        if (r * r == d) {
+            System.out.println(r);
+        } else if (s * s == f) {
+            System.out.println(s);
+        } else {
+            System.out.println(-1);
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] < col[j]) {
-                    result++;
-                }
-            }
-        }
-        return result;
     }
 }
